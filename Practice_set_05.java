cdf5f81bfc2a2d13803fbbfe92150c94490f7a03
Dinesh Behera <122552764+Dinesh-Behera@users.noreply.github.com>
2023-09-13 12:30:07 +0530

    Update Practice_set_05.java

--- Practice_set_05.java
+++ Practice_set_05.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Practice_set_05 {
     public static void main(String[] args) {
-         Problem No.1
-        Print this pattern
-        ****
-        ***
-        **
-        *
+        //  Problem No.1
+        // Print this pattern
+        // ****
+        // ***
+        // **
+        // *
         int n = 4;
         for (int i = n ; i > 0 ; i--){
             for (int j = 0 ; j < i ;j++){
@@ -15,8 +15,9 @@ public class Practice_set_05 {
             System.out.print("\n");
         }
 
-        Problem No.2
-                Write a program to sum first n even numbers using while loops
+        // Problem No.2
+        //         Write a program to sum first n even numbers using while loops
+        
         int sum = 0;
         int n = 4;
         int i = 0;
@@ -28,8 +29,9 @@ public class Practice_set_05 {
             System.out.print(sum);   // first 4 even numbers are 0 , 2 , 4 , 6
     }
 
-        Problem no.3
-                Write a program to print multiplication table of a given numbers n
+        // Problem no.3
+        //         Write a program to print multiplication table of a given numbers n
+    
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the multiplication table no. ");
         int n = sc.nextInt();
@@ -37,15 +39,17 @@ public class Practice_set_05 {
             System.out.println(n + " X " + i + " = " + n*i);
         }
 
-        Problem no.4
-                Write a program to print multiplication table of 10 in reverse order
+        // Problem no.4
+        //         Write a program to print multiplication table of 10 in reverse order
+
         int n = 10;
         for (int i = n ; i >= 1 ; i--){
             System.out.println(n + " X " + i + " = " + n*i);
         }
 
-        Problem no.5
-                Write a program to find factorial of a given number using for loop
+        // Problem no.5
+        //         Write a program to find factorial of a given number using for loop
+
                 int n = 5;
                 int factorial = 1;
                 for (int i = 1 ; i <= n ; i++){
@@ -53,9 +57,10 @@ public class Practice_set_05 {
                 }
         System.out.println("Factorial of " + n + "!" + " = " + factorial );
 
-       Problem no.6
-                Write a program to calculate the sum of the numbers occuring
-                in the multiplication table of 8
+       // Problem no.6
+       //          Write a program to calculate the sum of the numbers occuring
+       //          in the multiplication table of 8
+
         int n = 8 ;
         int sum = 0;
         for (int i = 1 ; i <= 10 ; i++){
